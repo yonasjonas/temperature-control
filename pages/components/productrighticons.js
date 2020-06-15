4a96b13c6b3e7aee45ee26dc7be1c6cec0b8731a
Jonas Samaitis <32172586+yonasjonas@users.noreply.github.com>
2020-06-15 22:19:27 +0100

    another try

--- pages/components/productrighticons.js
+++ pages/components/productrighticons.js
@@ -20,6 +20,7 @@ export default function ProductRight() {
         <div>
             <Container>
             {/* {(width >= 768) ? '<h3 className="mt-3 mb-3">Built-in Infrared Temperature Camera Face Recognition Terminal</h3>' : null } */}
+            <h3 className="mt-3 mb-3">Built-in Infrared Temperature Camera Face Recognition Terminal</h3>
                 <p>
                     Suitable for office areas, hotels, access gates, office buildings, schools, shopping malls, shops, communities, public service and management projects and other places where face access control is required.
                 </p>

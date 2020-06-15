4220cdb30e56469363b671899647e263423daeb7
Jonas Samaitis <32172586+yonasjonas@users.noreply.github.com>
2020-06-15 22:17:13 +0100

    iojio

--- pages/components/useWindowDimensions.js
+++ pages/components/useWindowDimensions.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+/* import { useState, useEffect } from 'react';
 
 
 
@@ -16,4 +16,4 @@ export default function useWindowDimensions() {
     }, []);
 
     return windowDimensions;
-}
+} */
